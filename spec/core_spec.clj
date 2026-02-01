907a63fee69bdf649b8ebdedd9fa645d025966de
YamaguchiKoki <shankouhanghui15@gmail.com>
2026-02-01 22:48:58 +0900

    init

---
+++ spec/core_spec.clj
@@ -0,0 +1,7 @@
+(ns ..core-spec
+  (:require [speclj.core :refer :all]
+            [..core :refer :all]))
+
+(describe "a test"
+  (it "FIXME, I fail."
+    (should= 0 1)))
---
+++ src/core.clj
@@ -0,0 +1,5 @@
+(ns ..core)
+
+(defn -main
+  [& args]
+  (println "Hello World"))
